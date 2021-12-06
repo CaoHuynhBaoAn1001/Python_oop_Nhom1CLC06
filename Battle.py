dbf02863436f25fd5d316473022e874020527381
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2021-12-06 18:33:57 +0700

    RPG Game

--- Battle.py
+++
@@ -1,8 +0,0 @@
-import Hero
-
-class Battle:
-	def do_battle(self,hero,enemy):
-		print("Hero face the {}".format(enemy.name))
-		while hero.Alive() and enemy.Alive():
-			hero.print_status()
-			enemy.print_status()
